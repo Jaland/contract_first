9c0801dacdafa3f834d145eb7979d2328c9584b8
James Land <hokie10@gmail.com>
2023-11-15 20:17:35 -0500

    Initial commit

---
+++ src/main/java/org/jland/TodoApiImpl.java
@@ -0,0 +1,42 @@
+package org.jland;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.jland.api.TodoApi;
+import org.jland.models.Todo;
+
+public class TodoApiImpl implements TodoApi {
+
+  @Override
+  public void createTodo(@Valid @NotNull Todo todo) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'createTodo'");
+  }
+
+  @Override
+  public void deleteTodo(Integer todoId) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'deleteTodo'");
+  }
+
+  @Override
+  public Todo getTodo(Integer todoId) {
+    return new Todo().completed(false).id(todoId).description("This is a test todo").name("Test Todo");
+  }
+
+  @Override
+  public List<Todo> getTodos(Boolean completed) {    
+    return Arrays.asList(new Todo().completed(false).id(12345).description("This is a test todo").name("Test Todo"));
+  }
+
+  @Override
+  public void updateTodo(Integer todoId, @Valid @NotNull Todo todo) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'updateTodo'");
+  }
+
+}
