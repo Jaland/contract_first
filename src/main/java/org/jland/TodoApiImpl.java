2c30c8de110df9f759ed7efb5cbdb7a0c65161be
James Land <hokie10@gmail.com>
2023-11-17 09:09:20 -0500

    Cleaning up contract

--- src/main/java/org/jland/TodoApiImpl.java
+++ src/main/java/org/jland/TodoApiImpl.java
@@ -25,12 +25,14 @@ public class TodoApiImpl implements TodoApi {
 
   @Override
   public Todo getTodo(Integer todoId) {
-    return new Todo().completed(false).id(todoId).description("This is a test todo").name("Test Todo");
+    return new Todo().completed(false).id(todoId).description("This is a test todo")
+    .description("Test Todo").author("Jamie Landusto");
   }
 
   @Override
   public List<Todo> getTodos(Boolean completed) {    
-    return Arrays.asList(new Todo().completed(false).id(12345).description("This is a test todo").name("Test Todo"));
+    return Arrays.asList(new Todo().completed(false).id(12345).description("This is a test todo")
+    .description("Test Todo").author("Jamie Landusto"));
   }
 
   @Override
